6df20e1b6b467835110a258b520291ded0c1bcda
ShahzadaAliHassan <reactjswebdev@gmail.com>
2021-12-26 22:37:28 +0500

    challenge book completed

--- class.js
+++ class.js
@@ -1,12 +1,29 @@
 class Book {
-  constructor(totalPagenumber, datePublished, reviewedBy, edition, authorName) {
-    this.totalPagenumber = totalPagenumber;
+  constructor(datePublished, stock, edition, authorName, title, price) {
+    this.title = title;
     this.datePublished = datePublished;
-    this.reviewedBy = reviewedBy;
+    this.stock = stock;
     this.edition = edition;
+    this.price = price;
     this.authorName = authorName;
   }
+  expensive() {
+    if (this.price > 100) {
+      return `book is expensive at ${this.price}$`;
+    } else {
+      return `book is not expensive at ${this.price}$`;
+    }
+  }
+  priceUpdates(newPrice) {
+    const oldPrice = this.price;
+    this.price = newPrice;
+    return `the new price is ${this.price}$, the old price was ${oldPrice}$`;
+  }
+  stockUpdates(newStock) {
+    const oldStock = this.stock;
+    return `the new stock is ${(this.stock +=
+      newStock)}, the old stock was ${oldStock}`;
+  }
 }
-export default class {
-  Book;
-}
+
+export default Book;
--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-import * as myModule from "/modules/my-module.js";
+import Book from "./class.js";
+const firstBook = new Book("12th Dec, 2020", 19, "3rd", "Rabia", "My Book", 22);
+
+console.log(firstBook);
+console.log(firstBook.expensive());
+console.log(firstBook.priceUpdates("111"));
+console.log(firstBook.expensive());
+console.log(firstBook.stockUpdates(10));
+
+document.getElementById("oldStock").innerHTML = firstBook.stock;
