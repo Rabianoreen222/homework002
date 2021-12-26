2e1f7370d60f52d89d7a32e5586228d0602e3689
ShahzadaAliHassan <reactjswebdev@gmail.com>
2021-12-26 21:42:13 +0500

    dataclear class book

--- class.js
+++ class.js
@@ -1,29 +1,12 @@
-class Bed {
-  constructor(
-    name,
-    size,
-    capacity,
-    color,
-    bedsheet,
-    matress,
-    usage,
-    age,
-    sidetable,
-    dateacquired
-  ) {
-    this.name = name;
-    this.size = size;
-    this.capacity = capacity;
-    this.color = color;
-    this.bedsheet = bedsheet;
-    this.matress = matress;
-    this.usage = usage;
-    this.age = age;
-    this.sidetable = sidetable;
-    this.dateacquired = dateacquired;
-  }
-  timeused(timestatus) {
-    return (this.age = timestatus);
+class Book {
+  constructor(totalPagenumber, datePublished, reviewedBy, edition, authorName) {
+    this.totalPagenumber = totalPagenumber;
+    this.datePublished = datePublished;
+    this.reviewedBy = reviewedBy;
+    this.edition = edition;
+    this.authorName = authorName;
   }
 }
-export default Bed;
+export default class {
+  Book;
+}
