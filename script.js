fa384cd2f3ff38b036fa8d4cd84f4d9798b47d17
ShahzadaAliHassan <reactjswebdev@gmail.com>
2021-12-27 00:27:02 +0500

    homework without cheating

---
+++ script.js
@@ -0,0 +1,9 @@
+function multiply(firstVar, secondVar) {
+  return firstVar * secondVar;
+}
+var firstvariable = { firstVar };
+var secondvariable = { secondVar };
+document.getElementById("firstVar").innerHTML;
+
+parseInt("1stvariable");
+parseInt("2ndvariable");
