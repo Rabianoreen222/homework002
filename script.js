bf2160fff53e1235c1db124a6f84092c642e34b2
ShahzadaAliHassan <reactjswebdev@gmail.com>
2021-12-28 12:55:37 +0500

    messed up

--- script.js
+++ script.js
@@ -1,9 +1,21 @@
-function multiply(firstVar, secondVar) {
+function multiply(firstVar, secondVar)=> {
   return firstVar * secondVar;
 }
-var firstvariable = { firstVar };
-var secondvariable = { secondVar };
-document.getElementById("firstVar").innerHTML;
+var firstvariable = `firstinputVar`;
+document.getElementById(firstinputVar).innerHTM=firstvariable;
+var secondvariable = `secondinputVar`;
+document.getElementById(secondinputVar).innerHTM=secondvariable;
+/* var secondvariable = { secondVar };
+let output = {multiply()}
+document.getElementById("firstVar").innerHTML=; */
 
 parseInt("1stvariable");
 parseInt("2ndvariable");
+document.getElementById("funcMultiply").onclick = multiply()};
+ document.getElementById("funcMultiply").innerHTML = "multiply()";
+}
+
+
+var random=``;
+document.getElementsByTagName("h2")[0].innerHTML=random;
+
